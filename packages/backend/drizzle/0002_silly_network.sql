c490f63ac02479949533554c094ac0e0a38babfa
enyineer <nico.enking@gmail.com>
2025-12-19 16:31:20 +0100

    Fix migration file
    Signed-off-by: enyineer <nico.enking@gmail.com>

--- packages/backend/drizzle/0002_silly_network.sql
+++ packages/backend/drizzle/0002_silly_network.sql
@@ -1,8 +1,8 @@
+DROP TABLE "role_permission";--> statement-breakpoint
+DROP TABLE "user_role";--> statement-breakpoint
 DROP TABLE "account";--> statement-breakpoint
 DROP TABLE "permission";--> statement-breakpoint
 DROP TABLE "role";--> statement-breakpoint
-DROP TABLE "role_permission";--> statement-breakpoint
 DROP TABLE "session";--> statement-breakpoint
 DROP TABLE "user";--> statement-breakpoint
-DROP TABLE "user_role";--> statement-breakpoint
 DROP TABLE "verification";
